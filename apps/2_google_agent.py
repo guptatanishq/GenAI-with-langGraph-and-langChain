1e9b6eac99cec1ae3a682509ebee5f25eba82b1e
guptatanishq <gupta.tanishq2004@gmail.com>
2026-08-29 16:15:35 +0530

    google search

--- apps/2_google_agent.py
+++ apps/2_google_agent.py
@@ -5,13 +5,16 @@ load_dotenv()
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
+from langgraph.checkpoint.memory import MemorySaver
 
 model = ChatGroq(model = 'openai/gpt-oss-20b')
 search = GoogleSerperAPIWrapper()
+memory = MemorySaver()
 
 agent = create_agent(
     model = model,
     tools = [search.run],
+    checkpointer = memory,
     system_prompt = "you are an agent and you can search any quesion on google"
 )
 
@@ -21,5 +24,8 @@ while True:
         print("good bye")
         break
 
-    response = agent.invoke({"messages": [{"role" : "user", "content" : query}]})
+    response = agent.invoke(
+        {"messages": [{"role" : "user", "content" : query}]},
+        {"configurable": {"thread_id": "abc123"}}
+        )
     print("AI: ", response["messages"][-1].content)
